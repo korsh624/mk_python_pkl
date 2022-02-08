428857fa349b29753901a92cb403a7bff292b7f0
Андрей Коршунов <korsh624@viro.local>
2022-02-08 16:27:00 +0300

    read img

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import cv2
 cap=cv2.VideoCapture(0)
 while (True):
-    ret,frame=cap.read()
+    # ret,frame=cap.read()
+    frame=cv2.imread("./apelsin.jpg")
+    frame=cv2.resize(frame,(800,600), interpolation=cv2.INTER_AREA)
     cv2.imshow("Frame", frame)
     if cv2.waitKey(1) == ord('q'):
         break
